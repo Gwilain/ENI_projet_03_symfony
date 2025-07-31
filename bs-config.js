3497c9b3e0f5adf4b7aaed0ba340ff2bc974f36c
Gwilain <ghislain.gillet@gmail.com>
2025-07-31 18:18:37 +0200

    Mise ne place du design, du système securité

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "files": [
+        "public/**/*.{html,js,css,php}",
+        "assets/styles/**/*.css"
+    ],
+    "proxy": "http://localhost:8000",
+    "reloadDelay": 1000,
+    "notify": false
+};
---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixture extends Fixture
+{
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    )
+    {
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_CA');
+
+        $user = new User();
+
+        $user->setEmail("admin@eni.fr");
+//        $user->setUsername("adminet");
+
+        $user->setRoles([ "ROLE_ADMIN"]);
+        $user->setPassword( $this->hasher->hashPassword( $user, "123456") );
+        $manager->persist($user);
+
+        for( $i = 0; $i < 10; $i++ ) {
+            $user2 = new User();
+            $user2->setEmail("user".$i."@eni.fr");
+//            $user2->setUsername("user".$i);
+
+            $user2->setRoles([ "ROLE_USER"]);
+            $user2->setPassword( $this->hasher->hashPassword( $user2, "123456") );
+            $manager->persist($user2);
+            $this->addReference("user".$i, $user2);
+
+        }
+
+        $manager->flush();
+    }
+}
